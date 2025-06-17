d384af0e83947e0ac12097f37c0ba4d3f3874586
Arpit Singh Bhatia <84636859+The-x-35@users.noreply.github.com>
2025-06-18 03:42:27 +0530

    discord button

--- app/games/[id]/page.tsx
+++ app/games/[id]/page.tsx
@@ -18,7 +18,6 @@ import { JUP_ULTRA_API } from '@/global/constant';
 import { getGameUrl } from '@/lib/utils';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import { useWalletModal } from '@solana/wallet-adapter-react-ui';
-import Image from "next/image";
 
 interface Game {
   id: string;
@@ -477,33 +476,20 @@ export default function GameDetailPage() {
                   <Copy className="h-3 w-3 sm:h-3 sm:w-3 md:h-3 md:w-3" />
                 )}
               </Button>
-              <a 
-                href="https://jup.ag/swap/So11111111111111111111111111111111111111112-SENDdRQtYMWaQrBroBrJ2Q53fgVuq95CV9UPGEvpCxa"
+              <a
+                href="https://discord.gg/sendarcade"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="inline-flex items-center justify-center h-7 w-7 sm:h-7 sm:w-7 md:h-7 md:w-7 rounded-md"
               >
-                <Image 
-                  src="https://jup.ag/favicon-32x32.png" 
-                  alt="Jupiter" 
-                  width={16}
-                  height={16}
-                  className="h-3 w-3 sm:h-3 sm:w-3 md:h-3 md:w-3"
-                />
-              </a>
-              <a 
-                href={`https://axiom.trade/t/${game.ca || ALPHA_GUI.SEND_TOKEN_CA}/@sendshot`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="inline-flex items-center justify-center h-7 w-7 sm:h-7 sm:w-7 md:h-7 md:w-7 rounded-md"
-              >
-                <Image 
-                  src="/icons/axiom.svg" 
-                  alt="Axiom" 
-                  width={16}
-                  height={16}
-                  className="h-4 w-4 sm:h-4 sm:w-4 md:h-4 md:w-4"
-                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="h-4 w-4 sm:h-4 sm:w-4 md:h-4 md:w-4 text-[#5865F2]"
+                >
+                  <path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0 12.64 12.64 0 0 0-.617-1.25.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057 19.9 19.9 0 0 0 5.993 3.03.078.078 0 0 0 .084-.028 14.09 14.09 0 0 0 1.226-1.994.076.076 0 0 0-.041-.106 13.107 13.107 0 0 1-1.872-.892.077.077 0 0 1-.008-.128 10.2 10.2 0 0 0 .372-.292.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127 12.299 12.299 0 0 1-1.873.892.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028 19.839 19.839 0 0 0 6.002-3.03.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.956-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.946 2.418-2.157 2.418z" />
+                </svg>
               </a>
             </div>
           </div>
