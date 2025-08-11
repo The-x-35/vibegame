4d2e75830749163100ec113cbca38e09faa39be9
Arpit Singh Bhatia <84636859+The-x-35@users.noreply.github.com>
2025-08-11 23:49:57 +0530

    footer fix

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -8,9 +8,6 @@ export default function Footer() {
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div className="md:col-span-2">
             <Logo />
-            <p className="mt-2 text-muted-foreground">
-              Low code solana, high speed innovation
-            </p>
           </div>
           
           <div>
